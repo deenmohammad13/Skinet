2e60a6e03ccf5ecb893765cc3e636b7806ddbc16
Deen Mohammad <deen9721@gmail.com>
2025-07-22 21:13:30 +0600

    Initial commit with .gitignore

---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using Infrastructure.Config;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfig).Assembly);
+        }
+    }
+}

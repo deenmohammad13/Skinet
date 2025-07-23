d134e9893faf58337bab86edb74371b4111eb326
Deen Mohammad <deen9721@gmail.com>
2025-07-23 14:52:02 +0600

    Creating Interface

--- Core/Interfaces/IProductRepository.cs
+++ Core/Interfaces/IProductRepository.cs
@@ -12,7 +12,7 @@ namespace Core.Interfaces
         Task<IReadOnlyList<Product>> GetProductsAsync();
         Task<Product?> GetProductByIdAsync(int id);
         void AddProduct(Product product);
-        void UpdateProduct(Product product);
+        void UpdateProduct(Product product);;
         void DeleteProduct(Product product);
         bool ProductExists(int id);
         Task<bool> SaveChangesAsync();

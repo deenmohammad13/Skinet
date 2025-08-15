560badd8fef0f2ea13396c6af9b4c02d3f2b86fe
Deen Mohammad <deen9721@gmail.com>
2025-08-15 12:54:47 +0600

    Creating Generic Repository

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 });
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
 var app = builder.Build();
 
---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T?> GetIdByAsync(int id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        void Add(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+        Task<bool> SaveChangesAsync();
+        bool Exists(int id);
+    }
+}

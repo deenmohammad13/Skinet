6b24d659e7140576a0e036b5c9a59a4814d45e7b
Deen Mohammad <deen9721@gmail.com>
2025-08-15 11:21:12 +0600

    Sorting by price

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -27,7 +27,8 @@ namespace Infrastructure.Data
             return await context.Products.FindAsync(id);
         }
 
-        public async Task<IReadOnlyList<Product>> GetProductsAsync(string? brand, string? type)
+        public async Task<IReadOnlyList<Product>> GetProductsAsync(string? brand,
+            string? type, string? sort)
         {
             var query = context.Products.AsQueryable();
 
@@ -39,6 +40,14 @@ namespace Infrastructure.Data
             {
                 query = query.Where(p => p.Type == type);
             }
+          
+            query = sort switch
+                {
+                    "priceAsc" => query.OrderBy(x => x.Price),
+                    "priceDesc" => query.OrderByDescending(x => x.Price),
+                    _ => query.OrderBy(x => x.Name)
+                };
+            
             return await query.ToListAsync();
         }
 

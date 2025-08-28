ada75dc874eb9f71fd242b521ffb1c0da891b514
Deen Mohammad <deen9721@gmail.com>
2025-08-28 12:43:22 +0600

    Using Types in our project

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
 import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/Models/Product';
+import { Pagination } from './shared/Models/Pagination';
 
 @Component({
   selector: 'app-root',
@@ -13,11 +15,11 @@ export class AppComponent implements OnInit {
   baseUrl = "https://localhost:5001/api/";
   private http = inject(HttpClient);
   title = 'Client';
-  products: any[] = [];
+  products: Product[] = [];
   //constructor(private http: HttpClient) {}  httpclient injection by constructor injrction
 
   ngOnInit(): void {
-      this.http.get<any>(this.baseUrl + 'products').subscribe({
+      this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
         next: response => this.products = response.data,
         error: error => console.log(error),
         complete: () => console.log('complete')

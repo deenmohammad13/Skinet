9152dd9f00248c474d764598b8592b0d5f4da1ee
Deen Mohammad <deen9721@gmail.com>
2025-09-02 12:23:26 +0600

    Adding Sort Functionality

--- Client/src/app/features/shop/shop.component.ts
+++ Client/src/app/features/shop/shop.component.ts
@@ -6,15 +6,19 @@ import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { FiltersDialogComponent } from './filters-dialog/filters-dialog.component';
-import { MatCard } from '@angular/material/card';
+import { MatListOption, MatSelectionList, MatSelectionListChange } from '@angular/material/list';
+import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-shop',
   imports: [
-   MatCard,
    MatButton,
    MatIcon,
    MatDialogModule,
+   MatSelectionList,
+   MatMenu,
+   MatMenuTrigger,  
+   MatListOption,
    ProductItemComponent
 ],
   templateUrl: './shop.component.html',
@@ -26,6 +30,12 @@ export class ShopComponent implements  OnInit {
      selectedTypes: string[] = [];
   private shopService = inject(ShopService);
   private dialogService = inject(MatDialog);
+  selectedsort = 'name';
+  sortOptions = [
+    {name: 'Alphabetical', value: 'name'},
+    {name: 'Price: Low to High', value: 'priceAsc'},
+    {name: 'Price: High to Low', value: 'priceDesc'},
+  ]
 
   ngOnInit(): void {
       this.initializeShops();
@@ -34,10 +44,21 @@ export class ShopComponent implements  OnInit {
   initializeShops(){
     this.shopService.getBrands();
     this.shopService.getTypes();
-    this.shopService.getProducts().subscribe({
+    this.getProducts();
+    
+  }
+  getProducts(){
+    this.shopService.getProducts(this.selectedBrands, this.selectedTypes, this.selectedsort).subscribe({
         next: response => this.products = response.data,
         error: error => console.log(error)
       })
+  }
+  onSortChange(event: MatSelectionListChange){
+    const selectedOption = event.options[0];
+    if (selectedOption){
+      this.selectedsort = selectedOption.value;
+      this.getProducts();
+    }
   }
 
   openFiltersDialog(){
@@ -54,10 +75,7 @@ export class ShopComponent implements  OnInit {
         if (result){
           this.selectedBrands = result.selectedBrands;
           this.selectedTypes = result.selectedTypes;
-          this.shopService.getProducts(this.selectedBrands, this.selectedTypes).subscribe({
-            next: response => this.products = response.data,
-            error: error => console.log(error)
-          })
+          this.getProducts();
         }
       }
     })

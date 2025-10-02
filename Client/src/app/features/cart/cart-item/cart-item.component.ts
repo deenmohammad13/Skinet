9255126761e5ece63bae11c06f8782b6b3f6c3b3
Deen Mohammad <deen9721@gmail.com>
2025-10-03 00:32:26 +0600

    styling the cart

---
+++ Client/src/app/features/cart/cart-item/cart-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, inject, input } from '@angular/core';
+import { CartService } from '../../../core/services/cart.service';
+import { CartItem } from '../../../shared/Models/cart';
+import { RouterLink } from '@angular/router';
+import { MatIconButton } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { CurrencyPipe } from '@angular/common';
+import { MatButton } from "@angular/material/button";
+
+@Component({
+  selector: 'app-cart-item',
+  imports: [RouterLink, MatIconButton, MatIconModule, CurrencyPipe, MatButton],
+  templateUrl: './cart-item.component.html',
+  styleUrl: './cart-item.component.scss'
+})
+export class CartItemComponent {
+  item = input.required<CartItem>();
+}

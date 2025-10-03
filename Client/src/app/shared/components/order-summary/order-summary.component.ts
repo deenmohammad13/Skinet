7d1bd7f5fc80855fda0b866932a0c4e117e96cae
Deen Mohammad <deen9721@gmail.com>
2025-10-03 12:56:56 +0600

    order summary part 2

--- Client/src/app/shared/components/order-summary/order-summary.component.ts
+++ Client/src/app/shared/components/order-summary/order-summary.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-order-summary',
-  imports: [],
+  imports: [MatButton,RouterLink,MatFormField,MatInput,MatLabel],
   templateUrl: './order-summary.component.html',
   styleUrl: './order-summary.component.scss'
 })
